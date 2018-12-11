aa103e2ae74f5dac588efa05cd7229046ec6defe
Lommy greenhands Reznak mo reznak <lreznak-@e3r8p4.21-school.ru>
2018-12-11 13:54:43 +0300

    raw version

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: lreznak- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 14:21:16 by lreznak-          #+#    #+#             */
-/*   Updated: 2018/12/10 15:26:34 by lreznak-         ###   ########.fr       */
+/*   Updated: 2018/12/11 13:54:27 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1
 # define FD_MAX 11000
 
 int         get_next_line(const int fd, char **line);
--- main.c
+++ main.c
@@ -1,41 +1,86 @@
 #include "get_next_line.h"
-
+#include "libft.h"
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <strings.h>
 
-int			*ft_memalloc_zero()
+static int			ft_strlen_bsn(char *str)
 {
-	int		*lc;
-	if ((lc = (int *)malloc(sizeof(int) * 512)))
+	int len;
+
+	len = 0;
+	while (*str && *str != '\n')
 	{
-		bzero(lc, 512);
-		return (lc);
+		len++;
+		str++;
 	}
-	return (NULL);
+	return (len);
 }
 
-int			get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
-	static char *buf[FD_MAX];
-	int i;
-	char *line;
+	static char			*buf[FD_MAX];
+	int					str_len;
+	char				*str1;
+	char				*str2;
+	char				*str3;
 
-	i = 0;
-:
-
-	linecount++;
+	str1 = ft_strnew(1);
+	str2 = ft_strnew(BUFF_SIZE);
+	if (!buf[fd])
+	{
+		if (read(fd, NULL, 0) == -1)
+			return (-1);
+		while (read(fd, str2, BUFF_SIZE))
+		{
+			str3 = str1;
+			if (!(str1 = ft_strjoin((const char *)str1, (const char *)str2)))
+				return (-1);
+			free(str3);
+		}
+		buf[fd] = str1;
+	}
+	if ((str_len = ft_strlen_bsn(buf[fd])))
+		*line = (char *)malloc(str_len + 1);
+	else
+	{
+		
+		return (0);
+	}
+	while (*buf[fd] && *buf[fd] != '\n')
+	{	
+		**line = *buf[fd];
+		buf[fd]++;
+		(*line)++;
+	}
+	**line = 0;
+	(*line) -= str_len;
+	if (*buf[fd] == '\n')
+		buf[fd]++;
 	return (1);
 }
 
 int			main(void)
 {
-	char src[100];
-	char *cp = src;
+	int n = 10230;
+	char *cp;
 	int fd = open("file", O_RDONLY);
-	while (read(fd, cp, BUFF_SIZE))
-		cp += BUFF_SIZE;
-	printf("%s", src);
+	/*printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	printf("%d\n", get_next_line(fd, &cp));
+	*/
+	while (n--)
+	{
+		printf("%d ---------- %d\n", open("file", O_RDONLY), n);
+	}
 }
