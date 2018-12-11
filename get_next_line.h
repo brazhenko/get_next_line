1875f03de39d9b8a888806a40a36909edf29f8a9
Lommy greenhands Reznak mo reznak <lreznak-@e3r7p6.21-school.ru>
2018-12-11 20:53:47 +0300

    row version

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: lreznak- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 14:21:16 by lreznak-          #+#    #+#             */
-/*   Updated: 2018/12/11 13:54:27 by lreznak-         ###   ########.fr       */
+/*   Updated: 2018/12/11 20:02:31 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10
 # define FD_MAX 11000
+# include "libft/libft.h"
 
-int         get_next_line(const int fd, char **line);
+int				get_next_line(const int fd, char **line);
 
 #endif
